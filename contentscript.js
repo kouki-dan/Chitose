9fc59ef1b17b9ab98ed3dc9bb34e30385a34b758
Kouki Saito <dan.addr.skd@gmail.com>
2013-04-07 19:30:49 +0900

    Buf fix

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 
-var children = document.getElementById("S_EKI_SEL");
-if(children != null){
-  children = children.childNodes;
+var selects = document.getElementsByTagName("select");
+for(var k = 0; k < selects.length; k++){
+  var children = selects[k].childNodes
   for(var i = 0; i < children.length; i++){
     var child = children[i];
     var text = child.innerText;
